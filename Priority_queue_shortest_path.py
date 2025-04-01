e17000c3c9591a769f83fb4747d10603629f36c5
NIhash69 <tejnihashrevu361@gmail.com>
2025-04-01 18:28:35 +0530

    kinda cleaner version

--- Priority_queue_shortest_path.py
+++ Priority_queue_shortest_path.py
@@ -26,23 +26,23 @@ def get_shortest_path(pres_node,end_node ):
         Priority_queue[pres_node] = 0 
         min_path=pres_node
 
-    print(Priority_queue)
+    #print(Priority_queue)
     for child_node,dist in Nodes_dict[pres_node].items():
         if child_node not in min_path:
             Priority_queue[min_path+child_node] = Priority_queue[min_path]+dist 
         
     del Priority_queue[min_path]
 
-    print(Priority_queue)
+    #print(Priority_queue)
     
     #let's get the min path
     min_path = min(Priority_queue,key=Priority_queue.get)
 
-    print('min_path',min_path)
+    #print('min_path',min_path)
 
     jump_node = min_path[-1]
 
-    print('jump_node',jump_node)
+    #print('jump_node',jump_node)
     if jump_node ==end_node: # If we end up jumping to the min path and that happend to be the end node, then just return. no extras
             return (min_path, Priority_queue[min_path])
     
@@ -127,6 +127,6 @@ connect_nodes(Nodes_dict,node7,node8,7)
 
 print(Nodes_dict)
 
-path,distance = get_shortest_path(node0,node4)
+path,distance = get_shortest_path(node0,node2)
 
 print('path',path,'distance',distance)
